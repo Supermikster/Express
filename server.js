cf3380acebf2bfab6d1b8f46501aeb5dbe47590c
Supermikster <31817855+Supermikster@users.noreply.github.com>
2020-07-29 21:05:50 +0300

    complete XHR connection to the server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const { ConnectionStates } = require('mongoose');
 const app = express();
 const expressWs = require('express-ws')(app);
@@ -6,7 +7,16 @@ const portNumber = 3000;
 const mongo = require('mongodb').MongoClient;
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
+const boduParser = require('body-parser');
 let connects = [];
+
+
+app.use(cors());
+app.use(boduParser.json());
+app.use(boduParser.urlencoded({extended: true}));
+app.use(cookieParser());
+app.use(session({secret: "My secret"}));
+
 
 mongo.connect('mongodb://localhost:27017/', {useNewUrlParser: true, useUnifiedTopology: true}, (err, client) =>{
 
@@ -22,7 +32,95 @@ mongo.connect('mongodb://localhost:27017/', {useNewUrlParser: true, useUnifiedTo
 
 });
 
-app.ws('/authorization', function(websocket, req){
+app.get('/authorization', function(req, res){
+    console.log('Client has been connected to the server and waiting authorizzation');
+    res.send('hello from the server');
+    
+});
+
+app.post('/authorization', function(req, res){
+    console.log(req.body);
+    const collection = req.app.locals.collection;
+    collection.findOne((req.body), function(err, result){
+        if(err){ 
+             return(console.log(err));
+        }if(result == null){
+            console.log('crete one');
+            res.sendStatus(400);
+        }else{
+            console.log(result);
+            res.sendStatus(200);
+        }
+    });
+});
+
+app.post('/registration', function(req, res){
+    res.cookie(req.body);
+    console.log(req.body);
+    const collection = req.app.locals.collection;
+    collection.findOne((req.body), function(err, result){
+        if(err){ 
+             return(console.log(err));
+        }if(result == null){
+            console.log('crete one');
+            collection.insert(req.body, function(err, result){
+                if (err)
+                    return(console.log(err));
+            });
+            res.sendStatus(200);
+            //res.redirect('/authorization');
+        }else{
+            console.log(result);
+            res.sendStatus(400);
+        }
+    });
+});
+
+
+
+/*app.ws('/echo', function(websocket,req){
+    console.log('A new client con!');
+    connects.push(websocket);
+            websocket.on('message',function(msg){
+                msg = JSON.parse(msg);
+                message = msg.data;
+                let name = data.name;
+                console.log(message);
+                const collection = req.app.locals.collection;
+                collection.findOne(({name: name}), function(err, result){
+                    if(err){
+                        return(console.log(err));
+                    }else{
+                        console.log(result);
+                    }
+                });
+                
+                //var clients = req;
+                connects.forEach(function e(client){
+                    if(client != websocket){ 
+                        client.send(message, name);
+                        client.send(JSON.stringify({
+                           // name: ws.personName,
+                            message: message
+                         }));
+                        }
+                        
+            });
+            collection.insert({name: name, message: message}, function(err, result){
+                if(err){
+                    return(console.log(err));
+                }
+            });
+    
+    });
+});*/
+
+
+
+
+app.listen(portNumber, ()=> {console.log('Server is running on port:3000')});
+
+/*app.ws('/authorization', function(websocket, req){
     console.log('Client has been connected to the server and waiting authorizzation');
         websocket.on('message', function(data){
             data = JSON.parse(data);
@@ -43,47 +141,9 @@ app.ws('/authorization', function(websocket, req){
  
             });
         }); 
-});
-
-/*app.ws('/echo', function(websocket,req){
-    console.log('A new client con!');
-    connects.push(websocket);
-            websocket.on('message',function(msg){
-                msg = JSON.parse(msg);
-                message = msg.data;
-                let name = data.name;
-                console.log(message);
-                const collection = req.app.locals.collection;
-                /*collection.findOne(({name: name}), function(err, result){
-                    if(err){
-                        return(console.log(err));
-                    }else{
-                        console.log(result);
-                    }
-                });
-                
-                //var clients = req;
-                connects.forEach(function e(client){
-                    if(client != websocket){ 
-                        client.send(message, name);
-                        /*client.send(JSON.stringify({
-                           // name: ws.personName,
-                            message: message
-                         }));
-                        }
-                        
-            });
-            collection.insert({name: name, message: message}, function(err, result){
-                if(err){
-                    return(console.log(err));
-                }
-            });
-    
-    });
 });*/
 
-
-app.ws('/registration', function(websocket, req){
+/*app.ws('/registration', function(websocket, req){
     console.log('Client has been connected to the server and waiting registration');
         websocket.on('message', function(data){
                 data = JSON.parse(data);
@@ -116,6 +176,4 @@ app.ws('/registration', function(websocket, req){
                 });
 
             }); 
-    });
-
-app.listen(portNumber, ()=> {console.log('Server is running on port:3000')});
+    });*/
